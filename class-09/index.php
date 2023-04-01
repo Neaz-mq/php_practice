45a1b5258077a9fcb5fbd60782b2186a8dd24686
Neaz-mq <neazmorshed666@gmail.com>
2023-04-01 20:15:17 +0600

    6th

---
+++ class-09/index.php
@@ -0,0 +1,12 @@
+<?php
+
+$name = "neaz_morshed";
+$number = 20;
+
+$is_admin = true;
+
+var_dump($name, $number);
+
+
+
+?>
---
+++ class-10/index.php
@@ -0,0 +1,7 @@
+<?php
+$name = "neaz";
+$school_name = "Dinajpur Zilla School";
+$name = ucwords($name);
+//echo "hello $name";
+printf("Hello %s, My school name is: %s", $name,$school_name );
+?>
